46152b85394ecf64dd08103fabe30d656c963f3a
mjbae11 <mikejbae@gmail.com>
2025-03-18 08:24:39 -0700

    setting up

--- styles/index.js
+++ styles/index.js
@@ -1,3 +1,26 @@
 'use strict';
+// Display
+const output = document.querySelector('h1');
+// Number buttons
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+const four = document.getElementById('four');
+const five = document.getElementById('five');
+const six = document.getElementById('six');
+const seven = document.getElementById('seven');
+const eight = document.getElementById('eight');
+const nine = document.getElementById('nine');
+const zero = document.getElementById('zero');
 
-const test = 'test';
+const display = {
+    
+};
+
+
+const buttons = {
+    // Method to handle number button clicks
+    numberButtonClick: function () {
+
+    }
+};
