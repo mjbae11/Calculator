5ffb8d960dcd782f7ce77114a3a0003aaa5906ab
mjbae11 <mikejbae@gmail.com>
2025-03-19 09:25:33 -0700

    trying to get operators to work

--- styles/index.js
+++ styles/index.js
@@ -13,14 +13,20 @@ const eight = document.getElementById("eight");
 const nine = document.getElementById("nine");
 const zero = document.getElementById("zero");
 const backspace = document.getElementById("backspace");
+// Basic operators
+const add = document.getElementById("add");
+const subtract = document.getElementById("subtract");
+const multiply = document.getElementById("multiply");
+const divide = document.getElementById("divide");
+
 const display = {
-    updateDisplay: function (number) {
+    updateDisplay: function (input) {
         // limit lenght of display
         if (output.textContent.length <= 10) {
             if (output.textContent == 0) {
                 output.textContent = "";
             }
-            output.textContent += number;
+            output.textContent += input;
         }
     },
 };
@@ -31,17 +37,45 @@ const buttons = {
         // get number from the button
         display.updateDisplay(event.target.textContent);
     },
+    // if there is nothing left replace with just a 0
     backspaceBtnClick: function () {
         output.textContent = output.textContent.slice(0, -1);
         if (output.textContent == "") {
             output.textContent = 0;
         }
     },
+    basicOperationBtnClick: function (event) {
+        // FIXME: this is just test console log
+        console.log(output);
+        // RegEx for matching "+ - * /"
+        // TODO: replace a previous operator with the new one, once the user clicks 2 in a row
+        // FIXME: enters if statement after 3 operators, not 2 for some reason.
+        if (output.textContent.slice(0, -1).match(/[+\-*/]/g)) {
+            // FIXME: this is just test console log
+            console.log("working?");
+        }
+        display.updateDisplay(event.target.textContent);
+    },
 };
-
-const allButtons = document.querySelectorAll("button");
+// Number button event listeners, gets the first 10 buttons
+const numberButtons = document.querySelectorAll("button");
 for (let i = 0; i < 10; i++) {
-    allButtons[i].addEventListener("click", buttons.numberButtonClick);
+    numberButtons[i].addEventListener("click", buttons.numberButtonClick);
 }
 
-backspace.addEventListener('click', buttons.backspaceBtnClick)
+// Backspace event listener
+backspace.addEventListener("click", buttons.backspaceBtnClick);
+
+// Basic operator listeners (repetitive)
+add.addEventListener("click", (event) => {
+    buttons.basicOperationBtnClick(event);
+});
+subtract.addEventListener("click", (event) => {
+    buttons.basicOperationBtnClick(event);
+});
+multiply.addEventListener("click", (event) => {
+    buttons.basicOperationBtnClick(event);
+});
+divide.addEventListener("click", (event) => {
+    buttons.basicOperationBtnClick(event);
+});
